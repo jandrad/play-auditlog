1219484ce532a9157bf984939f8030c0651747f3
José Andrade <jandrad@gmail.com>
2018-10-06 05:52:35 +0200

    Build version 1.0 compatible with PLay 1.4

--- src/play/modules/auditlog/AuditLog.java
+++ src/play/modules/auditlog/AuditLog.java
@@ -1,31 +1,30 @@
 package play.modules.auditlog;
 
+import java.util.List;
 import play.Logger;
 import play.Play;
 import play.utils.Java;
 
-import java.util.List;
-
 public class AuditLog {
 
-    public static Object invoke(String m, Object... args) {
-        Class auditLog = null;
-        try {
-            auditLog = Play.classloader.loadClass("controllers.auditlog.DefaultAuditLogEvents");
-        } catch (ClassNotFoundException e) {
-            Logger.error("class controllers.auditlog.DefaultAuditLogEvents not found");
-            return null;
-        }
-        List<Class> classes = Play.classloader.getAssignableClasses(auditLog);
-        if (classes.size() > 0) {
-            auditLog = classes.get(0);
-        }
-        try {
-            return Java.invokeStaticOrParent(auditLog, m, args);
-        } catch (Exception e) {
-            Logger.error(e, "Unable to invoke AuditLog method");
-            return null;
-        }
+  public static Object invoke(String m, Object... args) {
+    Class auditLog = null;
+    try {
+      auditLog = Play.classloader.loadClass("controllers.auditlog.DefaultAuditLogEvents");
+    } catch (ClassNotFoundException e) {
+      Logger.error("class controllers.auditlog.DefaultAuditLogEvents not found");
+      return null;
     }
+    List<Class> classes = Play.classloader.getAssignableClasses(auditLog);
+    if (classes.size() > 0) {
+      auditLog = classes.get(0);
+    }
+    try {
+      return Java.invokeStaticOrParent(auditLog, m, args);
+    } catch (Exception e) {
+      Logger.error(e, "Unable to invoke AuditLog method");
+      return null;
+    }
+  }
 
 }
